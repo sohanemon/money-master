7e59b9bb3033c719db543cbcffd2d18cbf40dbcc
sohanemon <m.sohanemon@gmail.com>
2022-08-23 23:24:47 +0600

    major problems fixed

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const $ = (id) => document.getElementById(id);
 const valueOf = (id) => parseFloat($(id).value);
 const textOf = (id) => parseFloat($(id).innerText);
-
+const giveAlert = (id) => {
+  if (isNaN(valueOf(id))) {
+    alert("ঠিকঠাক স্যালারি দে");
+    return true;
+  }
+  return false;
+};
 $("calculate-btn").addEventListener("click", (e) => {
   e.stopPropagation();
+  if (
+    giveAlert("income") ||
+    giveAlert("food-expenses") ||
+    giveAlert("rent-expenses") ||
+    giveAlert("clothes-expenses")
+  )
+    return;
   $("modal").style.display = "block";
   $("balance").innerText =
     valueOf("income") -
@@ -23,6 +36,7 @@ $("modal").addEventListener("click", (e) => {
   e.stopPropagation();
 });
 $("save-btn").addEventListener("click", () => {
+  if (giveAlert("saving-amount")) return;
   let x;
   $("savings").innerText = x =
     (valueOf("income") * valueOf("saving-amount")) / 100;
